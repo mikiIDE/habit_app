80816f025335f50cc90921cfb5b71aaf36b66851
mikiIDE <kinoko.miki@gmail.com>
2026-03-03 09:39:03 +0900

    feat: add habit list screen with FAB and add dialog

--- lib/presentation/habit_list_screen.dart
+++ lib/presentation/habit_list_screen.dart
@@ -10,6 +10,41 @@ class HabitListScreen extends StatefulWidget {
 class _HabitListScreenState extends State<HabitListScreen> {
   // ダミーデータ
   final List<String> _habits = ['運動', '読書', '瞑想'];
+  void _showAddHabitDialog() {
+    final textController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('習慣を追加'),
+          content: TextField(
+            controller: textController,
+            decoration: const InputDecoration(hintText: '習慣のタイトルを入力'),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('キャンセル'),
+            ),
+            TextButton(
+              onPressed: () {
+                final title = textController.text;
+                if (title.isNotEmpty) {
+                  setState(() {
+                    _habits.add(title);
+                  });
+                }
+                Navigator.pop(context);
+              },
+              child: const Text('追加'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +56,7 @@ class _HabitListScreenState extends State<HabitListScreen> {
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // TODO: ダイアログを表示する
-        },
+        onPressed: _showAddHabitDialog,
         child: const Icon(Icons.add),
       ),
     );
